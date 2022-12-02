2e6e621d8a3fb9e30e0e05c0bdca8401c5ff5872
Jonny Heggheim <hegjon@gmail.com>
2022-12-02 20:06:49 +0100

    Refactor day2 part1 of lua

--- day02/part1.lua
+++ day02/part1.lua
@@ -26,19 +26,31 @@ local draws = {
   Z="C"
 }
 
-local total = 0
-
-for line in io.lines() do
-  local opponent = string.sub(line, 1, 1)
-  local selected = string.sub(line, 3, 3)
-
-  total = total + points[selected]
-  if wins[selected] == opponent then
-    total = total + 6
-  elseif draws[selected] == opponent then
-    total = total + 3
-  end
-
+function select_score(selected)
+  return points[selected]
 end
 
-print(total)
+function match_score(opponent, selected)
+  if wins[selected] == opponent then
+    return 6
+  elseif draws[selected] == opponent then
+    return 3
+  else
+    return 0
+  end
+end
+
+function total_score(lines)
+  local total = 0
+  for line in lines do
+    local opponent = string.sub(line, 1, 1)
+    local selected = string.sub(line, 3, 3)
+
+    total = total +
+            select_score(selected) +
+            match_score(opponent, selected)
+  end
+  return total
+end
+
+print(total_score(io.lines()))
